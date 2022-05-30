76d1cc6b5b84466e85a0252c8db33345849febad
angry-mech <angry_mech@protonmail.com>
2022-05-30 15:03:33 +0200

    add metis contracts

---
+++ factory_contracts/metis/HyperGovTokenPlaceholder.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.12;
+
+contract HyperGovTokenPlaceholder {
+    function totalSupply() public  pure returns (uint256) {
+        return 0;
+    }
+    function totalBurn() public  pure  returns (uint256) {
+        return 0 ;
+    }
+    function balanceOf(address account) public pure returns (uint256) {
+        return 0;
+    }
+    function currentBurnPercent() public pure returns (uint256) {
+        return 0;
+    }
+    function getCurrentTwap() public pure returns (uint256) {
+        return 0;
+    }
+    function getLastTwap() public pure returns (uint256) {
+        return 0;
+    }
+}
